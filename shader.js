f81babe5e8f1216c4b3b8a4c311e9d95d3a77ee7
Tarek Ali <tarekali15@outlook.com>
2020-09-07 20:25:29 -0500

    formatting

--- shader.js
+++ shader.js
@@ -1,25 +1,19 @@
 function setup() {
-    createCanvas(window.innerWidth, window.innerHeight);
-  }
-  
-  function draw() {
-      
-    if (mouseIsPressed) {
-      fill(0);
-      background(255)
-      circle(mouseX, mouseY, 80);
-    }
-    
-  }
-  class particle {
-constructor(){
-    //position 
-    this.pos = createVector(random(0,window.innerWidth,random(0,innerHeight)))
-
+  createCanvas(window.innerWidth, window.innerHeight);
 }
 
-
-
-
-
+function draw() {
+  if (mouseIsPressed) {
+    fill(0);
+    background(255);
+    circle(mouseX, mouseY, 80);
   }
+}
+class particle {
+  constructor() {
+    //position
+    this.pos = createVector(
+      random(0, window.innerWidth, random(0, innerHeight))
+    );
+  }
+}
