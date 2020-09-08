5ccde9bf494ef65f570d855ae5bb13a5877cf1d8
Jack Bocresion <jackbocresion@Jacks-iMac-Pro.local>
2020-09-07 21:25:07 -0400

    added react native project

--- shader.js
+++ shader.js
@@ -1,25 +1,25 @@
+let p;
 function setup() {
-    createCanvas(window.innerWidth, window.innerHeight);
-  }
-  
-  function draw() {
-      
-    if (mouseIsPressed) {
-      fill(0);
-      background(255)
-      circle(mouseX, mouseY, 80);
-    }
-    
-  }
-  class particle {
-constructor(){
-    //position 
-    this.pos = createVector(random(0,window.innerWidth,random(0,innerHeight)))
-
+  createCanvas(window.innerWidth, window.innerHeight);
+  p = new Particle();
 }
 
-
-
-
-
+function draw() {
+  if (mouseIsPressed) {
+    fill(0);
+    background(255);
+    circle(mouseX, mouseY, 80);
   }
+}
+class Particle {
+  constructor() {
+    //position
+    //width and height are native variables in p5
+    this.pos = createVector(random(0, width, random(0, height)));
+    this.size = 10;
+  }
+  draw = () => {
+    fill(0, 0, 0, 255);
+    circle(this.pos.x, this.pos.y, this.size);
+  };
+}
