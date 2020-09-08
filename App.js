9d91e8fbf8f76f9ddb2860341574d5d6e212351d
Tarek Ali <tarekali15@outlook.com>
2020-09-08 10:41:32 -0500

    added game file and button + event handler lets get this bread

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import Game from './utils/Game';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Button title="Start?" style={styles.button}></Button>
+      <Button title="Start?" style={styles.button} onPress={Game()}></Button>
       <StatusBar style="auto" />
     </View>
   );
