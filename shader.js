4bf9ccedf356df6a9fed4c91e710ea8596306dcf
Jack Bocresion <jackbocresion@Jacks-iMac-Pro.local>
2020-09-07 21:48:48 -0400

    removed some clutter

--- shader.js
+++ shader.js
@@ -1,11 +1,13 @@
 let p;
 function setup() {
-  createCanvas(width, height);
+  createCanvas(window.innerWidth, window.innerHeight);
   p = new Particle();
 }
 
 function draw() {
-    p.draw()
+    background(220)
+    p.update()
+  p.draw();
 }
 class Particle {
   constructor() {
@@ -13,9 +15,22 @@ class Particle {
     //width and height are native variables in p5
     this.pos = createVector(random(0, width), random(0, height));
     this.size = 10;
+    this.vel = createVector(random(-2, 2), random(-2, 2));
   }
   draw = () => {
     fill(0, 0, 0, 255);
     circle(this.pos.x, this.pos.y, this.size);
   };
+  update = () => {
+    this.pos.add(this.vel);
+    this.edges()
+  };
+  edges = () => {
+    if(this.pos.x<0 || this.pos.x >width){
+        this.vel.x*=-1
+    }
+    if(this.pos.y<0 || this.pos.y >height){
+        this.vel.y*=-1
+    }
+  }
 }
