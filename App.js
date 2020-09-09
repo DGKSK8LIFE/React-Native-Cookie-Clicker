a17f32b9eb402002f388b7d78a506343eaccc7d0
Jack Bocresion <jackbocresion@Jacks-iMac-Pro.local>
2020-09-09 19:44:29 -0400

    added stylesheet in json

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import Game from './utils/Game';
-
+import "./styles.json"
 export default function App() {
   return (
     <View style={styles.container}>
@@ -12,17 +12,4 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  button: {
-    flex: 1,
-    backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create(styles.json);
