584b41520b8970161da4529ade0d516a3ddfd87b
Tarek Ali <tarekali15@outlook.com>
2020-09-09 11:33:57 -0500

    added circle generation

--- utils/GenerateCircles.js
+++ utils/GenerateCircles.js
@@ -1,5 +1,16 @@
+import { View, StyleSheet } from 'react-native';
+import React from 'react';
 import P5Wrapper from 'react-p5-wrapper';
 
 export default function SketchCircle(p) {
-
+  return <View style={styles.circle}></View>;
 }
+
+const styles = StyleSheet.create({
+  circle: {
+    width: 44,
+    height: 44,
+    borderRadius: 44 / 2,
+    backgroundColor: 'red',
+  },
+});
