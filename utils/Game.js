dd01246f55847461c334d5d7c7170f7bec86fbbb
Tarek Ali <tarekali15@outlook.com>
2020-09-10 10:50:48 -0500

    simpler file structure

--- utils/Game.js
+++ utils/Game.js
@@ -1,8 +1,23 @@
+import { View, StyleSheet, Button } from 'react-native';
 import React from 'react';
-import SketchCircle from './GenerateCircles';
+import P5Wrapper from 'react-p5-wrapper';
 
-export default function Game() {
+export default function Game(p) {
+  const [score, setScore] = useState(0);
+
   return (
-    <SketchCircle />
+    <View>
+      <h1>Score: {score}</h1>
+      <Button onPress={() => setScore(score + 1)} style={styles.circle} />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  circle: {
+    width: 84,
+    height: 84,
+    borderRadius: 84 / 2,
+    backgroundColor: 'red',
+  },
+});
