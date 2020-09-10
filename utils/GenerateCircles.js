e858e602c92fb767e79d4c6dbe714b09fa95fa56
Tarek Ali <tarekali15@outlook.com>
2020-09-10 10:48:26 -0500

    added sketchcircle with state management and scoring; will add random position/random generation later

--- utils/GenerateCircles.js
+++ utils/GenerateCircles.js
@@ -1,9 +1,20 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Button } from 'react-native';
 import React from 'react';
 import P5Wrapper from 'react-p5-wrapper';
 
 export default function SketchCircle(p) {
-  return <View style={styles.circle}></View>;
+
+  const [ score, setScore ] = useState(0);
+
+  return (
+    <View>
+      <h1>Score: { score }</h1>
+      <Button 
+        onPress={ () => setScore(score + 1)}
+        style={styles.circle}
+      />
+    </View> 
+  );
 }
 
 const styles = StyleSheet.create({
