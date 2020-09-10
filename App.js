8bb55361624988fde7ad6de59896a685682f12a7
Jack Bocresion <jackbocresion@Jacks-iMac-Pro.local>
2020-09-10 15:56:38 -0400

    allowed circle and score to move freely on screen

--- App.js
+++ App.js
@@ -2,21 +2,30 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import Game from './utils/Game';
-import  CirclePos from "./utils/Circles"
+// import  CirclePos from "./utils/Circles"
+import { Dimensions } from 'react-native';
+
+const { width } = Dimensions.get('window');
+const { height } = Dimensions.get('window');
 export default function App() {
   return (
     <View style={styles.container}>
-        <Game/>
+      <Game />
       <StatusBar style="auto" />
     </View>
   );
 }
+const posStyle = StyleSheet.create({
+  position: {},
+});
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    paddingLeft: width * Math.random(),
+    paddingTop: height * Math.random(),
   },
 });
