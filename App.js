7e0cbe2d7843124661e31c3c38334f66e2fb1f75
Tarek Ali <tarekali15@outlook.com>
2020-09-11 10:33:23 -0500

    removed shit

--- App.js
+++ App.js
@@ -8,35 +8,7 @@ const width = Dimensions.get('window');
 const height = Dimensions.get('window');
 
 export default function App() {
-  function createStyles() {
-    const styles = StyleSheet.create({
-      paddingLeft: (85 + width) * Math.random() - 400,
-      paddingTop: (height + 100) * Math.random() - 420,
-    });
-    return styles;
-  }
-  const [score, setScore] = useState(0);
-  const [show, setShow] = useState([false, true]);
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>Score: {score}</Text>
-      <View style={createStyles()}>
-        <Game onHide={() => handleHide(0)} shown={show[0]} />
-      </View>
-      <View style={createStyles()}>
-        <Game onHide={() => handleHide(1)} shown={show[1]} />
-      </View>
-
-      {/* <View style={createStyles()}>
-        <Game onHide={() => handleHide(1)} shown={show[1]} />
-      </View> */}
-    </SafeAreaView>
-  );
-  function handleHide(index) {
-    const circles = [...show];
-    setScore(score + 1);
-    setShow([!show[0], !show[1]]);
-  }
+  
 }
 
 const styles = StyleSheet.create({
