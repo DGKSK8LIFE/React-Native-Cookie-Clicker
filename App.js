9fce3f4d372ac1e08644919f768161c36a4b4eab
Tarek Ali <tarekali15@outlook.com>
2020-09-12 14:53:36 -0500

    styles

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: '#121011',
     alignItems: 'center',
     justifyContent: 'center',
   },
