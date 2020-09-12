69e4b4185bd922594a556f1de1156b3b9aa1e84d
Tarek Ali <tarekali15@outlook.com>
2020-09-11 20:04:45 -0500

    made the button actually work

--- utils/Game.js
+++ utils/Game.js
@@ -1,16 +1,16 @@
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import React, { useState } from 'react';
 
 export default function Game() {
   const [score, setScore] = useState(0);
 
   return (
-    <View style={styles.CircleShapeView}>
-      <View
-        onClick={() => setScore(score + 1)}
+    <View>
+      <Text style={styles.h1}>Score: {score}</Text>
+      <TouchableOpacity
+        onPress={() => setScore(score + 1)}
         style={styles.CircleShapeView}
       />
-      <Text style={styles.h1}>Score: {score}</Text>
     </View>
   );
 }
@@ -21,9 +21,10 @@ const styles = StyleSheet.create({
     width: 84,
     height: 84,
     borderRadius: 84 / 2,
-    backgroundColor: '#FF00FF',
+    backgroundColor: '#FF0000',
   },
   h1: {
-    fontSize: 20
+    fontSize: 20,
+    fontWeight: "bold"
   },
 });
